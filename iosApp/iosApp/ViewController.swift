b985e75c7d952ec2d2d0b8e69de4dacbdabcbe0b
MohsinNajmuddin <mohsin.najmuddin@elastica.co>
2018-08-03 07:57:07 +0300

    Done

--- iosApp/iosApp/ViewController.swift
+++ iosApp/iosApp/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
 
@@ -20,6 +21,9 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    
+    
+    
+    
 }
 
